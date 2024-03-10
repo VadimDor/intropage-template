123e94fb2ce0db1e6c2056ac60a937919c334dfd
Vadim Dor <V.Dorez@mail.ru>
2024-03-10 15:49:37 +0300

    Create prepare_gist_metrics.sh

---
+++ .github/prepare_gist_metrics.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+while getopts a:n:u:d: flag
+do
+    case "${flag}" in
+        a) author=${OPTARG};;
+        n) name=${OPTARG};;
+        u) urlname=${OPTARG};;
+        d) description=${OPTARG};;
+    esac
+done
+
+echo "Author: $author";
+echo "Project Name: $name";
+echo "Project URL name: $urlname";
+echo "Description: $description";
+
+echo "Preparing gist..."
+
+gh api \
+  --method PUT \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  /repos/$author/$repo_name/actions/secrets/METRICS_TOKEN \
+  -f encrypted_value='c2VjcmV0' \
+  -f key_id='012345678912345678'
+echo "Configured secret METRICS_TOKEN"
